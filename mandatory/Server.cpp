a2ac141d00ff7cf5a0573f8133b6297dce2fb7e9
Alejandro Vicinay Estrade <alvicina@c3r4s4.42urduliz.com>
2024-07-12 11:13:35 +0200

    movido directorio cgi al directorio root del server

--- mandatory/Server.cpp
+++ mandatory/Server.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   Server.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: afidalgo <afidalgo@student.42.fr>          +#+  +:+       +#+        */
+/*   By: alvicina <alvicina@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/26 12:27:41 by alvicina          #+#    #+#             */
-/*   Updated: 2024/07/03 16:51:30 by alvicina         ###   ########.fr       */
+/*   Updated: 2024/07/12 11:12:00 by alvicina         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -356,7 +356,8 @@ bool & methodsFlag, bool & autoIndexFlag, bool & maxSizeFlag)
 
 void Server::checkLocationCgiIndex(Location & location)
 {
-	std::string path = location.getLocationRoot() + location.getLocationPath() + "/" + location.getIndexLocation();
+	std::string path = getRoot() + location.getLocationPath() + "/" + location.getIndexLocation();
+	//std::string path = location.getLocationRoot() + location.getLocationPath() + "/" + location.getIndexLocation();
 	if (Utils::typeOfFile(path) != 1)
 	{
 		char *cwd = getcwd(NULL, 0);
