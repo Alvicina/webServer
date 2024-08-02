ca88bd554f61a0083219690ae50a58c0d1d2a29a
Jose Mari Torralba Soto <jotorral@c3r4s2.42urduliz.com>
2024-08-02 09:07:17 +0200

    delete listo

--- mandatory/RequestHandlerDELETE.cpp
+++ mandatory/RequestHandlerDELETE.cpp
@@ -40,41 +40,6 @@ RequestHandlerDelete& RequestHandlerDelete::operator=(RequestHandlerDelete & oth
 	return (*this);
 }
 
-/*
-Response* RequestHandlerDelete::doHandleRequest(void)
-{
-	Response *response = new Response();
-
-	// ********** Deletes file if possible **********
-
-	if (isRequestMethodAllow() == false)
-		throw HandlerErrorException(405, *_request);
-	std::string	pathAndFile = strPathAndFile();
-	int err = std::remove(pathAndFile.c_str()); //Deletes the file
-	if (err != 0)
-	{
-		delete response;
-		int errnum = this->fileError(pathAndFile);
-		throw HandlerErrorException(errnum, *_request);
-		//std::cerr << "Error: Unable to delete the file " << pathAndFile << std::endl;
-	}
-	else
-	{
-		//std::cout << "El fichero " << pathAndFile << " fue borrado exitosamente.\n";
-		std::string content = pathAndFile + " has been successfully deleted!!";  
-		response->setContent(content);
-		response->setProtocol(_request->getProtocol());
-		response->setProtocolVersion(_request->getProtocolVersion());
-		response->ResponseHeaderRoutine(*response, _request);
-		response->setStatusCode(200);
-		std::string statusCodeMessage = Utils::codeStatus(response->getStatusCode());
-		response->setStatusCodeMessage(statusCodeMessage);
-		response->ResponseRawRoutine();
-	}
-	return (response);
-}
-*/
-
 Response* RequestHandlerDelete::doHandleRequest(void)
 {
 	Response	*response = new Response();
@@ -99,11 +64,9 @@ Response* RequestHandlerDelete::doHandleRequest(void)
 		{
 			int errnum = this->fileError(pathAndFile);
 			exceptionRoutine(errnum, response);
-			//std::cerr << "Error: Unable to delete the file " << pathAndFile << std::endl;
 		}
 		else
 		{
-			//std::cout << "El fichero " << pathAndFile << " fue borrado exitosamente.\n";
 			std::string content = pathAndFile + " has been successfully deleted!!";
 			response->setContent(content);
 			response->setProtocol(_request->getProtocol());
@@ -137,55 +100,3 @@ int RequestHandlerDelete::fileError(std::string pathAndFile)
 	return (403); // If it exist, 403 forbidden error
 }
 
-// Returns "path + file" used to delete considering configuration of location and server
-/*std::string	RequestHandlerDelete::strPathAndFile() // ****** ELIMINAR PORQUE NO SE USA ******
-{
-	std::string path;
-	std::string file;
-	std::string pathAndFile = _request->getUri();
-
-	if (_request->getLocation()) // if location exists
-	{
-		std::string::size_type pos = pathAndFile.find_last_of('/');
-		if (pos != std::string::npos && pos != pathAndFile.length() - 1)
-		{
-    	    path = pathAndFile.substr(0, pos + 1);
-			file = pathAndFile.substr(pos + 1);
-    	}
-		else
-		{
-			path = pathAndFile;
-		}
-
-		std::string substPath = _request->getLocation()->getReturnLocation();
-		if (!substPath.empty()) // if _return exists in location
-		{
-			return ("301");
-		}
-		substPath = _request->getLocation()->getAliasLocation();
-		if (!substPath.empty()) // if _alias exists in location
-		{
-			if (substPath[substPath.size() - 1] == '/' && file[0] == '/')
-				file.erase(0, 1);
-			if (substPath[0] != '/')
-				substPath.insert(0, "/");
-			return ('.' + substPath + file);
-		}
-		substPath = _request->getLocation()->getLocationRoot(); // root of location
-		if (!substPath.empty()) // if _root exists in location
-		{
-			if (substPath[substPath.size() - 1] == '/' && pathAndFile[0] == '/')
-				pathAndFile.erase(0, 1);
-			if (substPath[0] != '/')
-				substPath.insert(0, "/");
-			return ('.' + substPath + pathAndFile);
-		}
-	}
-	std::string root = _request->getServer()->getRoot();
-	if (root[root.size() - 1] == '/' && pathAndFile[0] == '/')
-		pathAndFile.erase(0, 1);
-	if (root[0] != '/')
-		root.insert(0, "/");
-//	std::string prueba = '.' + root + pathAndFile;
-	return ('.' + root + pathAndFile); //root of Server class
-}*/
